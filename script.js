d608c9a2a001c28ef1192ce21b371ae4a01f2c99
Victor Marczyk <marczykv@gmail.com>
2023-07-18 20:58:07 -0400

    functionality for api

--- script.js
+++ script.js
@@ -4,155 +4,169 @@ const recipeListElement = document.getElementById('recipe-list');
 const recipeDetailsElement = document.getElementById('recipe-details');
 const autocompleteListElement = document.getElementById('autocomplete-list');
 const historyBlock = document.getElementById('history-block');
-const apiKey = 'a0d37f1dc6853215adb58e3a7e45f9d5';
+const apiKey = 'a0d37f1dc6853215adb58e3a7e45f9d5'; 
+const appId = '3d90a8ed'; 
+
+const autocompleteSuggestions = [
+  'Chicken',
+  'Salad',
+  'Smoothie',
+  'Burger',
+  'Ice Cream',
+  'Pineapple',
+  'Strawberry',
+];
 
 let searchHistory = [];
 
 searchForm.addEventListener('submit', handleFormSubmit);
-searchInput.addEventListener('input', handleInputChange);
+// searchInput.addEventListener('input', handleInputChange);
 
 function handleFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const searchTerm = searchInput.value.trim();
-    if (searchTerm.length > 0) {
-        searchRecipes(searchTerm);
-        saveSearchToHistory(searchTerm);
-    }
+  const searchTerm = searchInput.value.trim();
+  console.log(searchTerm)
+  if (searchTerm.length > 0) {
+    searchRecipes(searchTerm);
+    saveSearchToHistory(searchTerm);
+  }
 }
 
 function handleInputChange() {
-    const searchTerm = searchInput.value.trim();
-    if (searchTerm.length > 0) {
-        autocompleteSearch(searchTerm);
-    } else {
-        clearAutocompleteList();
-    }
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm.length > 0) {
+    autocompleteSearch(searchTerm);
+  } else {
+    clearAutocompleteList();
+  }
 }
 
 function searchRecipes(searchTerm) {
-    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchTerm}&app_id=3d90a8ed&app_key=${a0d37f1dc6853215adb58e3a7e45f9d5}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(response => response.json())
-        .then(data => {
-            displayRecipes(data.hits);
-        })
-        .catch(error => {
-            console.log('An error occurred:', error);
-        });
+  var URL = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchTerm}&app_id=3d90a8ed&app_key=${apiKey}`
+  console.log(URL)
+  fetch(URL, {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+  })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+          displayRecipes(data.hits);
+          saveSearchToHistory(searchTerm);
+      })
+      .catch(error => {
+          console.log('An error occurred:', error);
+      });
 }
 
 function displayRecipes(recipes) {
-    // Clear previous search results
-    recipeListElement.innerHTML = '';
+  // Clear previous search results
+  recipeListElement.innerHTML = '';
 
-    // Iterate through the recipes and create a recipe element for each
-    recipes.forEach(recipe => {
-        const recipeElement = document.createElement('div');
-        recipeElement.classList.add('recipe');
-        recipeElement.textContent = recipe.recipe.label;
+  // Iterate through the recipes and create a recipe element for each
+  recipes.forEach(recipe => {
+    const recipeElement = document.createElement('li');
+    recipeElement.classList.add('recipe');
+    recipeElement.textContent = recipe.recipe.label;
 
-        // Add a click event listener to show recipe details when clicked
-        recipeElement.addEventListener('click', () => {
-            showRecipeDetails(recipe.recipe);
-        });
-
-        // Append the recipe element to the recipe list
-        recipeListElement.appendChild(recipeElement);
+    // Add a click event listener to show recipe details when clicked
+    recipeElement.addEventListener('click', () => {
+      showRecipeDetails(recipe.recipe);
     });
+
+    // Append the recipe element to the recipe list
+    recipeListElement.appendChild(recipeElement);
+  });
 }
 
 function showRecipeDetails(recipe) {
-    // Clear previous recipe details
-    recipeDetailsElement.innerHTML = '';
+  // Clear previous recipe details
+  //recipeDetailsElement.innerHTML = '';
+  console.log(recipe)
+  // Create elements for the recipe details
+  const recipeTitle = document.createElement('h2');
+  recipeTitle.textContent = recipe.label;
 
-    // Create elements for the recipe details
-    const recipeTitle = document.createElement('h2');
-    recipeTitle.textContent = recipe.label;
+  const imgEl = document.createElement("img")
+  // imgEl.setAttribute("src",recipe.images.REGULAR.url)
+  imgEl.setAttribute("src",recipe.image)
 
-    const ingredientsTitle = document.createElement('h3');
-    ingredientsTitle.textContent = 'Ingredients';
+  const ingredientsTitle = document.createElement('h3');
+  ingredientsTitle.textContent = 'Ingredients';
 
-    const ingredientsList = document.createElement('ul');
-    recipe.ingredientLines.forEach(ingredient => {
-        const ingredientItem = document.createElement('li');
-        ingredientItem.textContent = ingredient;
-        ingredientsList.appendChild(ingredientItem);
-    });
+  const ingredientsList = document.createElement('ul');
+  recipe.ingredientLines.forEach(ingredient => {
+    const ingredientItem = document.createElement('li');
+    ingredientItem.textContent = ingredient;
+    ingredientsList.appendChild(ingredientItem);
+  });
 
-    const stepsTitle = document.createElement('h3');
-    stepsTitle.textContent = 'Steps';
+  const stepsTitle = document.createElement('h3');
+  stepsTitle.textContent = 'Steps';
 
-    const stepsList = document.createElement('ol');
-    recipe.steps.forEach((step, index) => {
-        const stepItem = document.createElement('li');
-        stepItem.textContent = `Step ${index + 1}: ${step}`;
-        stepsList.appendChild(stepItem);
-    });
-
-    // Append the elements to the recipe details
-    recipeDetailsElement.appendChild(recipeTitle);
-    recipeDetailsElement.appendChild(ingredientsTitle);
-    recipeDetailsElement.appendChild(ingredientsList);
-    recipeDetailsElement.appendChild(stepsTitle);
-    recipeDetailsElement.appendChild(stepsList);
+  const stepsList = document.createElement('ol');
+  recipe.ingredients.forEach((ing, index) => {
+    const stepItem = document.createElement('li');
+    stepItem.textContent = `ingredients ${index + 1}: ${ing.text} - ${ing.quantity}`;
+    stepsList.appendChild(stepItem);
+  });
+  console.log(recipeDetailsElement)
+  // Append the elements to the recipe details
+  recipeDetailsElement.innerHTML = ""
+  recipeDetailsElement.appendChild(recipeTitle);
+  recipeDetailsElement.appendChild(imgEl)
+  recipeDetailsElement.appendChild(ingredientsTitle);
+  recipeDetailsElement.appendChild(stepsList);
+  // recipeDetailsElement.appendChild(stepsTitle);
+  // recipeDetailsElement.appendChild(ingredientsList);
 }
 
 function autocompleteSearch(searchTerm) {
-    // Clear previous autocomplete suggestions
-    clearAutocompleteList();
+  // Clear previous autocomplete suggestions
+  clearAutocompleteList();
 
-    // Make an API call to fetch autocomplete suggestions
-    fetch(`https://api.example.com/autocomplete?q=${searchTerm}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(response => response.json())
-        .then(data => {
-            displayAutocompleteSuggestions(data.suggestions);
-        })
-        .catch(error => {
-            console.log('An error occurred:', error);
-        });
+  // Filter the suggestions based on the search term
+  const filteredSuggestions = autocompleteSuggestions.filter(suggestion =>
+    suggestion.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
+
+  displayAutocompleteSuggestions(filteredSuggestions);
 }
 
 function displayAutocompleteSuggestions(suggestions) {
-    // Create elements for the autocomplete suggestions
-    suggestions.forEach(suggestion => {
-        const suggestionItem = document.createElement('div');
-        suggestionItem.classList.add('autocomplete-item');
-        suggestionItem.textContent = suggestion;
+  // Create elements for the autocomplete suggestions
+  suggestions.forEach(suggestion => {
+    const suggestionItem = document.createElement('div');
+    suggestionItem.classList.add('autocomplete-item');
+    suggestionItem.textContent = suggestion;
 
-        // Add a click event listener to set the suggestion as the search input value
-        suggestionItem.addEventListener('click', () => {
-            searchInput.value = suggestion;
-            handleFormSubmit(event); // Perform search when suggestion is clicked
-            clearAutocompleteList(); // Clear the autocomplete list after search
-        });
-
-        // Append the suggestion element to the autocomplete list
-        autocompleteListElement.appendChild(suggestionItem);
+    // Add a click event listener to set the suggestion as the search input value
+    suggestionItem.addEventListener('click', () => {
+      searchInput.value = suggestion;
+      handleFormSubmit(event); // Perform search when suggestion is clicked
+      clearAutocompleteList(); // Clear the autocomplete list after search
     });
+
+    // Append the suggestion element to the autocomplete list
+    autocompleteListElement.appendChild(suggestionItem);
+  });
 }
 
 function clearAutocompleteList() {
-    autocompleteListElement.innerHTML = '';
+  autocompleteListElement.innerHTML = '';
 }
 
 function saveSearchToHistory(searchTerm) {
-    searchHistory.push(searchTerm);
-    // Update the historyBlock element with the search history
-    historyBlock.innerHTML = '';
-    searchHistory.forEach(search => {
-        const searchItem = document.createElement('div');
-        searchItem.classList.add('searchItem');
-        searchItem.textContent = search;
-        historyBlock.appendChild(searchItem);
-    });
+  searchHistory.push(searchTerm);
+  // Update the historyBlock element with the search history
+  historyBlock.innerHTML = '';
+  searchHistory.forEach(search => {
+    const searchItem = document.createElement('div');
+    searchItem.classList.add('searchItem');
+    searchItem.textContent = search;
+    historyBlock.appendChild(searchItem);
+  });
 }
